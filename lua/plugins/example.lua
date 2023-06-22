d618b07f6e2900bdff87e20be766a2ec230d2c8c
funzd <funzdpro1@gmail.com>
2023-06-21 22:03:18 -0600

    *maintained options set in treesitter*

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -146,12 +146,9 @@ return {
   --{ import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- add more treesitter parsers
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = {
-  --     ensure_installed = "maintained",
-  --   },
-  -- },
+  -- { "nvim-treesitter/nvim-treesitter", opts = {
+  --   ensure_installed = "maintained",
+  -- } },
 
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
