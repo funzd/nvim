a226d32c482b636169c6f742673ba6f5f73e42b6
funzd <funzdpro1@gmail.com>
2023-06-29 22:39:43 -0600

    *themes.lua file changed*

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,1 @@
-return {
-  {
-    "sonph/onehalf",
-    name = "onehalf",
-  },
-  -- {
-  --   "mhartington/oceanic-next",
-  --   name = "oceanic-next",
-  -- },
-  {
-    "LazyVim/LazyVim",
-    opt = {
-      colorscheme = "onehalf",
-    },
-  },
-}
+return {}
