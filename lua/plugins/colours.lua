e74c4d722ee9892c356e2ec13f9899fc8a648f59
funzd <funzdpro1@gmail.com>
2023-11-16 16:18:47 -0600

    *issues fixed*

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -5,6 +5,8 @@ return {
   },
   {
     "NvChad/nvim-colorizer.lua",
+    event = { "BufReadPre", "BufNewFile" },
+    config = true,
     opts = { user_default_options = { tailwind = true } },
   },
   {
--- lua/plugins/custom_plugins.lua
+++ lua/plugins/custom_plugins.lua
@@ -1,22 +1,61 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
     config = function()
-      require("nvim-treesitter.configs").setup({
-        -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        -- ensure_installed = "",
-        prefer_git = true,
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
+
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
         highlight = {
-          enable = true, -- false will disable the whole extension
-          disable = {}, -- list of language that will be disabled
-        },
-
-        indent = {
-          enable = false,
-        },
-
-        rainbow = {
           enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        autotag = {
+          enable = true,
+        },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
         },
       })
     end,
@@ -30,99 +69,119 @@ return {
       require("auto-save").setup({ enabled = true })
     end,
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- change color for arrows in tree to light blue
+      vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+      vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC555 ]])
+
+      -- configure nvim-tree
+      nvimtree.setup({
+        view = {
+          width = 35,
+          relativenumber = true,
+        },
+        -- change folder arrow icons
+        renderer = {
+          indent_markers = {
+            enable = true,
+          },
+          icons = {
+            glyphs = {
+              folder = {
+                arrow_closed = "", -- arrow when folder is closed
+                arrow_open = "", -- arrow when folder is open
+              },
+            },
+          },
+        },
+        -- disable window_picker for
+        -- explorer to work well with
+        -- window splits
+        actions = {
+          open_file = {
+            window_picker = {
+              enable = false,
+            },
+          },
+        },
+        filters = {
+          custom = { ".DS_Store" },
+        },
+        git = {
+          ignore = false,
+        },
+      })
+
+      -- set key ffmaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+      keymap.set(
+        "n",
+        "<leader>ef",
+        "<cmd>NvimTreeFindFileToggle<CR>",
+        { desc = "Toggle file explorer on current file" }
+      ) -- toggle file explorer on current file
+      keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+      keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    end,
+  },
   -- web-dev-icon -> config
   {
     "nvim-tree/nvim-web-devicons",
-    opt = true,
-    dependencies = {
-      "DaikyXendo/nvim-material-icon",
-      config = function()
-        require("nvim-web-devicons").setup({
-          override = require("nvim-material-icon").get_icons(),
-        })
-      end,
-    },
-  },
-  --git-> plugins
+    config = function()
+      require("nvim-web-devicons").set_icon({
+        gql = {
+          icon = "",
+          color = "#e535ab",
+          cterm_color = "199",
+          name = "GraphQL",
+        },
+      })
+    end,
+  }, --git-> plugins
   {
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
+    event = { "BufReadPre", "BufNewFile" },
+    config = true,
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    setup = require("telescope").setup({
-      defaults = {
-        preview = {
-          mime_hook = function(filepath, bufnr, opts)
-            local is_image = function(filepath)
-              local image_extensions = { "png", "jpg", "jpeg", "gif" } -- Supported image formats
-              local split_path = vim.split(filepath:lower(), ".", { plain = true })
-              local extension = split_path[#split_path]
-              return vim.tbl_contains(image_extensions, extension)
-            end
-            if is_image(filepath) then
-              local term = vim.api.nvim_open_term(bufnr, {})
-              local function send_output(_, data, _)
-                for _, d in ipairs(data) do
-                  vim.api.nvim_chan_send(term, d .. "\r\n")
-                end
-              end
-              vim.fn.jobstart({
-                "viu",
-                "-w",
-                "40",
-                "-b",
-                filepath,
-              }, {
-                on_stdout = send_output,
-                stdout_buffered = true,
-              })
-            else
-              require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
-            end
-          end,
-        },
-      },
-      extensions = {
-        media_files = {
-          -- filetypes whitelist
-          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-          filetypes = { "png", "webp", "jpg", "jpeg", "gif" },
-          -- find command (defaults to `fd`)
-          find_cmd = "rg",
-        },
-        arecibo = {
-          ["selected_engine"] = "google",
-          ["url_open_command"] = "xdg-open",
-          ["show_http_headers"] = false,
-          ["show_domain_icons"] = false,
-        },
-      },
-    }),
-  },
-  -- add telescope-media-files
-  {
-    "telescope.nvim",
+    branch = "0.1.x",
     dependencies = {
-      "nvim-telescope/telescope-media-files.nvim",
-      config = function()
-        require("telescope").extensions.media_files.media_files()
-      end,
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          path_display = { "truncate " },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+              ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    end,
   },
-  -- {
-  --   "telescope.nvim",
-  --   dependencies = {
-  --     "nvim-telescope/telescope-arecibo.nvim",
-  --     build = "make",
-  --     rocks = { "openssl", "lua-http-parser" },
-  --     config = function()
-  --       require("telescope").load_extension("arecibo")
-  --     end,
-  --   },
-  -- },
 }
